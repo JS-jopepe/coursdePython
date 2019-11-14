6f24e5c6b4349ea3a0759afca117fc9a09dad2a5
JS-jopepe <jovenel.s@outlook.com>
2019-11-13 21:43:33 -0300

    Quarta 13/11/2019 f

--- 1pf/2pf.py
+++ 1pf/2pf.py
@@ -3,4 +3,7 @@ def somar():
     number2 = float(input("digite o numero 2"))
     return = number1 * number2
 
+def dividir(number1=10, number2=2):
+    return = number1 / number2
 print(somar())
+print(dividir())
